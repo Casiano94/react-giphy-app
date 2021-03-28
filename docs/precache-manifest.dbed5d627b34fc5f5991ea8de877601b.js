9b84271daf3ff9e9437454b3b579d1603275e3b2
Orlando Martinez <orlandomartinez.94@gmail.com>
2021-03-27 23:15:38 -0300

    Add docs

---
+++ docs/precache-manifest.dbed5d627b34fc5f5991ea8de877601b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d980b0a6f9bb4ee29bd73fb3283761",
+    "url": "/index.html"
+  },
+  {
+    "revision": "419a193ce06beb84a57d",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419a193ce06beb84a57d",
+    "url": "/static/js/main.bd570e58.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
